8593f0a2de365acf8f3c2054fdb639ea33627de5
Adam G Horn <hornadam8@gmail.com>
2020-04-05 15:20:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,27 @@ require_relative "../config/environment.rb"
 
 class Student
 
-  att
+  attr_accessor :name,:grade
+  attr_reader :id
+  
+  def intitialize(id=nil,name grade)
+    @id = id
+    @name = name
+    @grade = grade
+  end
+  
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE students (
+      id INTEGER PRIMARY KEY
+      name TEXT
+      grade TEXT)
+    SQL
+    
+    DB[:conn].execute(sql)
+  end
+  
+  def self.drop_table
 
 
 end
